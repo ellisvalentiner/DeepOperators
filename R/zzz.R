67d04b538e6ffbd43ed4018e919fad118854f1d4
Ellis Valentiner <ellisvalentiner@gmail.com>
2019-04-21 19:08:28 +0000

    Export the package env

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
-.onAttach <- function(libname, pkgname) {
+#' Deep Operators
+#' @export
+deepoperators <- new.env()
+
+.onLoad <- function(libname, pkgname) {
   deepoperators.env <<- new.env()
   or_model <- keras::unserialize_model(readRDS(system.file("models", "or.rds", package = "DeepOperators", mustWork = TRUE)))
   and_model <- keras::unserialize_model(readRDS(system.file("models", "and.rds", package = "DeepOperators", mustWork = TRUE)))
